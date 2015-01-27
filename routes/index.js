e5d49b4ca4e41a519526e0fc090a4e4af40882f8
Nicolas Suarez <nicolassuarez.cabrera@gmail.com>
2015-01-27 00:25:16 -0200

    Fix style and add github api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
-
+var gitHub =  require('../lib/github_api');
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+
+  posts =  []
+
+  gitHub.repos(function(repos){
+    for (var i = repos.length - 1; i >= 0; i--) {
+      post = repos[i];
+      gitHub.getPost(post.name, function(content){
+        post.content = content;
+        console.log(post.content);
+        posts.push(post);
+        res.render('index', { title: 'Express' , posts: posts });
+      });
+    };
+  });
+
 };
+
